2889014efbe0caaf1e1430e7aeab0ae3aec30e46
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-04 22:27:32 +0700

    second commit

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -1,28 +1,28 @@
 @if ($message = Session::get('success'))
     <div id="one" class="alert alert-success alert-block">
         <strong>{{ $message }}</strong>
-        <button id="btn" type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top"/></button>
+        <button id="btn" type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top2"/></button>
 
     </div>
 @endif
 
 @if ($message = Session::get('error'))
     <div class="alert alert-danger alert-block">
-        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top"/></button>
+        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top2"/></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
 
 @if ($message = Session::get('warning'))
     <div class="alert alert-warning alert-block">
-        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top"/></button>
+        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top2"/></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
 
 @if ($message = Session::get('info'))
     <div class="alert alert-info alert-block">
-        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top"/></button>
+        <button type="button" class="close" onclick="myFunction2()"><x-ri-close-line class="w-6 h-6 mt-3 text-red-600 transform-top2"/></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
