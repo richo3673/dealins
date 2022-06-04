f9cc38ea5d762b00fe64177ed3f1163c548cea5f
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-04 21:49:11 +0700

    second commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
+    <script src="/js/app.js"></script>
     <script type="text/javascript" src="/js/jquery.js"></script>
     <script type="text/javascript" src="/js/jquery.js"></script>
 
