d528a48dcf9bb2512a5e79d3972745281b7c7f56
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-04 13:42:00 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,13 @@ Route::get('/mine', [MainController::class, 'byUserId'])->middleware(['auth'])->
 Route::post('/mine', [MainController::class, 'byUserId'])->middleware(['auth'])->name('mine');
 Route::get('/riwayat', [MainController::class, 'riwayat'])->middleware(['auth'])->name('riwayat');
 
-Route::get('/dealins/{id}/edit', [MainController::class, 'edit'])->middleware(['auth'])->name('edit-form');
+Route::get('/{id}/edit', [MainController::class, 'edit'])->middleware(['auth'])->name('edit-form');
 Route::get('/create', [MainController::class, 'create'])->middleware(['auth'])->name('create-form');
-Route::post('/dealins/{id}/update', [MainController::class, 'update'])->middleware(['auth'])->name('update');
+Route::post('/{id}/update', [MainController::class, 'update'])->middleware(['auth'])->name('update');
 Route::post('/add', [MainController::class, 'store'])->middleware(['auth'])->name('add');
-Route::get('/dealins/{id}', [MainController::class, 'show'])->name('show');
-Route::get('/dealins/{id}/delete', [MainController::class, 'delete'])->middleware(['auth'])->name('delete');
-Route::get('/dealins/', [MainController::class, 'search'])->name('search');
+Route::get('/{id}', [MainController::class, 'show'])->name('show');
+Route::get('/{id}/delete', [MainController::class, 'delete'])->middleware(['auth'])->name('delete');
+Route::get('/', [MainController::class, 'search'])->name('search');
 Route::get('/', [MainController::class, 'home'])->middleware(['auth'])->name('dashboard');
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/profile', [MainController::class, 'showProfile'])->middleware(['auth'])->name('profile');
