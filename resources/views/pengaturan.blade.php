e80e438e0861f4a22d88741dfa7810904d10b391
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-05 18:01:11 +0700

    commit set

--- resources/views/pengaturan.blade.php
+++ resources/views/pengaturan.blade.php
@@ -26,9 +26,8 @@
                 <div class="add-child">
                     <div class="input-group2">
                         @method('delete')
-                        <form method="POST" data-action="{{ route('hapus_riwayat') }}" id="add-user-form"
+                        <form method="get" data-action="{{ route('hapus_riwayat') }}"
                               enctype="multipart/form-data">
-                            @csrf
                             <x-button class="h-10 text-base text-red-600">hapus</x-button>
                         </form>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/profile', [MainController::class, 'showProfile'])->middleware(['auth'])->name('profile');
 Route::post('/profile/update', [MainController::class, 'updateProfile'])->middleware(['auth'])->name('update_profile');
 Route::get('/pengaturan', [MainController::class, 'pengaturan'])->middleware(['auth'])->name('pengaturan');
-Route::post('/pengaturan/hapus_riwayat', [MainController::class, 'hapusRiwayat'])->middleware(['auth'])->name('hapus_riwayat');
+Route::get('/pengaturan/hapus_riwayat', [MainController::class, 'hapusRiwayat'])->middleware(['auth'])->name('hapus_riwayat');
 Route::post('/pengaturan/hapus_akun', [MainController::class, 'deleteUser'])->middleware(['auth'])->name('hapus_akun');
 
 
