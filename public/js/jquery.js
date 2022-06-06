8e32f920ba1087ff46fdab71b48e0fecdca60bda
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-06 17:46:04 +0700

    commit set

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -2,9 +2,8 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('i')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
