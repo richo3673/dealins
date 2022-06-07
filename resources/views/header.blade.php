902d72982f5c9b259aed37ed8166dc09cfd43c5a
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-07 11:10:34 +0700

    commit set

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -104,15 +104,14 @@
                 </div>
             @else
                 <div id="myDropdown" class="dropdown-content">
-                    <a href="{{ url('/login') }}">
-                        Login
-{{--                        @csrf--}}
-{{--                        <x-dropdown-link :href="route('login')"--}}
-{{--                                         onclick="event.preventDefault();--}}
-{{--                                                this.closest('form').submit();">--}}
-{{--                            <p><i class="fa-solid fa-right-to-bracket"></i>&emsp;Log In</p>--}}
-{{--                        </x-dropdown-link>--}}
-                    </a>
+                    <form method="GET" action="{{ route('login') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('login')"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            <p><i class="fa-solid fa-right-to-bracket"></i>&emsp;Log In</p>
+                        </x-dropdown-link>
+                    </form>
                     <form method="GET" action="{{ route('register') }}">
                         @csrf
                         <x-dropdown-link :href="route('register')"
