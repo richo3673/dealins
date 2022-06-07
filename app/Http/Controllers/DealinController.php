6d39f18c2924ba259f49dc6eb87263a83fcf0d5f
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-07 23:00:22 +0700

    final commt

--- app/Http/Controllers/DealinController.php
+++ app/Http/Controllers/DealinController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class DealinController extends Controller
 {
