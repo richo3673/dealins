49f5e129e968ff50cdb6f5160cf05a28fbe9e47d
Richo Wijaya <richowijayaputra3673@gmail.com>
2022-06-07 10:53:42 +0700

    commit set

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
